5f390e4e0b6d57e4b965b06cccc559f311989b2c
abdiwakmitiku <abdiwakmitiku2nd@gmail.com>
2025-06-09 14:27:39 +0300

    connecting to database

---
+++ server/db/dbConfig.js
@@ -0,0 +1,17 @@
+const mysql2 = require("mysql2");
+
+const dbConnection = mysql2.createPool({
+  user: "evangadi-user",
+  database: "evangadi-form",
+  host: "localhost",
+  password: "123456",
+  connectionLimit: 10,
+});
+
+// dbConnection.execute("select 'test'", (err, result) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log(result);
+//   }
+// });
